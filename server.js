5ba14a5b4f6f0c27b9c9903ddf60900f83872cb4
Minh Vũ <minhnc2k4@gmail.com>
2026-06-02 04:17:08 +0700

    chatbot

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 // Khởi tạo AI với API Key của bạn
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const aiModel = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
+const aiModel = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
 
 const app = express();
